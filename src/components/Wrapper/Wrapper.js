98c3ec978758f80a3588e0d900b5e4d655a24898
Ken Tillman <anson_125@yahoo.com>
2018-05-02 13:12:12 -0400

    corrections

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "./Wrapper.css";
 
-const Wrapper = props => (
-    <div className="row">
-        <div className="wrapper ">{props.children}</div>
-    </div>
-);
+const Wrapper = props => <div className="wrapper">{props.children}</div>;
 
 export default Wrapper;
